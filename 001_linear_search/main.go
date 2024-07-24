e16835613f1b346f12eeea5f364c7e75c7fba032
Leonidas Menendez <leonidasmenendez@gmail.com>
2024-07-24 12:05:19 -0600

    Added linearSearchGeneric

--- 001_linear_search/main.go
+++ 001_linear_search/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// linearSearchGeneric given an array (haystack) of 'comparable' type and a needle of the same type
+// returns the index of value present in the array or -1 if not found
+func linearSearchGeneric[K comparable](arr []K, item K) int {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == item {
+			return i
+		}
+	}
+	return -1
+}
+
 // linearSearch2 given an array (haystack) and an integer (needle),
 // returns all indexes of the needle present in the array
 func linearSearch2(arr []int, target int) []int {
@@ -29,5 +40,10 @@ func linearSearch(arr []int, target int) int {
 func main() {
 	fmt.Println(linearSearch([]int{1, 3, 5, 6}, 2))
 	fmt.Println(linearSearch([]int{1, 3, 5, 6}, 5))
+
 	fmt.Println(linearSearch2([]int{1, 3, 5, 6, 5}, 5))
+
+	fmt.Println(linearSearchGeneric([]int{1, 3, 5, 6}, 2))
+	fmt.Println(linearSearchGeneric([]int{1, 3, 5, 6}, 5))
+
 }
