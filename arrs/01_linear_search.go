42999e94b42e59be3b802a623bfb5ebdd12fac99
Leonidas Menendez <leonidasmenendez@gmail.com>
2025-04-01 05:57:51 -0600

    reorganize the folder project

--- arrs/01_linear_search.go
+++ arrs/01_linear_search.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-// linearSearchGeneric given an array (haystack) of 'comparable' type and a needle of the same type
+// genericLinearSearch given an array (haystack) of 'comparable' type and a needle of the same type
 // returns the index of value present in the array or -1 if not found
-func linearSearchGeneric[K comparable](arr []K, item K) int {
+func genericLinearSearch[K comparable](arr []K, item K) int {
 	for i := 0; i < len(arr); i++ {
 		if arr[i] == item {
 			return i
@@ -13,10 +13,10 @@ func linearSearchGeneric[K comparable](arr []K, item K) int {
 	return -1
 }
 
-// linearSearch2 given an array (haystack) and an integer (needle),
+// linearSearchAll given an array (haystack) and an integer (needle),
 // returns all indexes of the needle present in the array
-func linearSearch2(arr []int, target int) []int {
-	result := []int{}
+func linearSearchAll(arr []int, target int) []int {
+	var result []int
 	for i := 0; i < len(arr); i++ {
 		if arr[i] == target {
 			result = append(result, i)
@@ -41,9 +41,12 @@ func main() {
 	fmt.Println(linearSearch([]int{1, 3, 5, 6}, 2))
 	fmt.Println(linearSearch([]int{1, 3, 5, 6}, 5))
 
-	fmt.Println(linearSearch2([]int{1, 3, 5, 6, 5}, 5))
+	fmt.Println(linearSearchAll([]int{1, 3, 5, 6, 5}, 5))
 
-	fmt.Println(linearSearchGeneric([]int{1, 3, 5, 6}, 2))
-	fmt.Println(linearSearchGeneric([]int{1, 3, 5, 6}, 5))
+	fmt.Println(genericLinearSearch([]int{1, 3, 5, 6}, 2))
+	fmt.Println(genericLinearSearch([]int{1, 3, 5, 6}, 5))
+
+	fmt.Println(genericLinearSearch([]string{"bambo", "number", "five"}, "five"))
+	fmt.Println(genericLinearSearch([]bool{false, false, false, true}, true))
 
 }
